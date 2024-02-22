ddff4b0bacdbb7309badabb6e2708589cd082ef6
taylorlea43 <taylor.lea@quinnipiac.edu>
2024-02-22 12:59:22 -0500

    user is asked which animal they would like to be cat or dog and the program replies with art here

--- animalpicker.java
+++ animalpicker.java
@@ -1,6 +1,21 @@
+import java.util.Scanner;
+
 public class animalpicker{
 public static void main(String[] args) {
     
+    Scanner scanner = new Scanner(System.in);
+    System.out.println("Would you like to see a cat or a dog? type 'c' for cat or 'd for dog:");
+    String animal = scanner.next();
+
+    if(animal.contains("c")){
+        System.out.println("CAT ASCII ART HERE");
+    }else if(animal.contains("d")){
+        System.out.println("DOG ASCII ART HERE");
+    }else{
+        System.out.println("error: animal not found");
+    }
+    
+
 }
 
 }
